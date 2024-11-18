6b55ab83ecf6cd4e8f2f29c3f72a1088f754207b
Malek <106095300+MalekD5@users.noreply.github.com>
2024-11-18 03:24:13 +0300

    refactor: base schema for auth

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,20 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import type { InferSelectModel } from "drizzle-orm";
+import { serial, integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-	id: integer(),
-	firstName: varchar("first_name"),
+export const userTable = pgTable("user", {
+	id: serial("id").primaryKey(),
 });
+
+export const sessionTable = pgTable("session", {
+	id: text("id").primaryKey(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date",
+	}).notNull(),
+});
+
+export type User = InferSelectModel<typeof userTable>;
+export type Session = InferSelectModel<typeof sessionTable>;
