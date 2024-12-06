d5dfd86bafbed551f220af074d5532c696e2e69b
Malek <106095300+MalekD5@users.noreply.github.com>
2024-12-07 00:00:48 +0300

    refactor(database): initial auth schema setup

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
 import { Pool } from "pg";
+import * as schema from "./schema";
 
 export const client = new Pool({
 	connectionString: process.env.DB_URL as string,
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,24 @@
 import type { InferSelectModel } from "drizzle-orm";
-import { serial, integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	boolean,
+	integer,
+	pgTable,
+	serial,
+	text,
+	timestamp,
+} from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
 	id: serial("id").primaryKey(),
+	email: text("email").notNull(),
+	displayName: text("display_name").notNull(),
+	hashedPassword: text("hashed_password").notNull(),
+	createdAt: timestamp("created_at", {
+		withTimezone: true,
+		mode: "date",
+	}).defaultNow(),
+	totpKey: text("totp_key"),
+	verified: boolean("verified_email").default(false),
 });
 
 export const sessionTable = pgTable("session", {
@@ -10,6 +26,29 @@ export const sessionTable = pgTable("session", {
 	userId: integer("user_id")
 		.notNull()
 		.references(() => userTable.id),
+	twoFactorVerified: boolean("two_factor_verified").default(false),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date",
+	}).notNull(),
+});
+
+export const recoveryCodeTable = pgTable("recovery_code", {
+	code: text("code").primaryKey(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => userTable.id),
+});
+
+const passwordResetSessionTable = pgTable("password_reset_session", {
+	id: text("id").primaryKey(),
+	userId: integer("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	email: text("email").notNull(),
+	code: text("code").notNull(),
+	emailVerified: boolean("email_verified").default(false),
+	twoFactorVerified: boolean("two_factor_verified").default(false),
 	expiresAt: timestamp("expires_at", {
 		withTimezone: true,
 		mode: "date",
@@ -18,3 +57,7 @@ export const sessionTable = pgTable("session", {
 
 export type User = InferSelectModel<typeof userTable>;
 export type Session = InferSelectModel<typeof sessionTable>;
+export type PasswordResetSession = InferSelectModel<
+	typeof passwordResetSessionTable
+>;
+export type RecoveryCode = InferSelectModel<typeof recoveryCodeTable>;
